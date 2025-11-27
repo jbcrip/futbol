9cb4ae5e5adc6d812dbbdc52b2eea04540c43673
SrDexcrip <dex.crip.228@gmail.com>
2025-11-26 20:05:46 -0600

    Fix: Usar HashRouter para GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import Navegacion from './components/navegacion'
 import Home from './pages/Home'
 import Login from './pages/Login'
